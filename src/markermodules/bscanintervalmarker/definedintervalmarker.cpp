77e22dcbce49c98d87a9a225bb3b863fb6e2e9fa
Kay Gawlik <kay.gawlik@beuth-hochschule.de>
2017-04-10 10:27:24 +0200

    Intervallfarben (Qualitätsfehler) korrigiert
    - Josef seine Vorgaben aus BeCRF

--- src/markermodules/bscanintervalmarker/definedintervalmarker.cpp
+++ src/markermodules/bscanintervalmarker/definedintervalmarker.cpp
@@ -27,13 +27,13 @@ DefinedIntervalMarker::DefinedIntervalMarker()
 
 	IntervalMarker qualityFailures("qualityFailures", tr("quality failures").toStdString());
 
-	qualityFailures.addMarker(IntervalMarker::Marker("signalmissing" , tr("signal missing"          ).toStdString(),   0, 140, 140));
-	qualityFailures.addMarker(IntervalMarker::Marker("ILM_upper"     , tr("ILM upper"               ).toStdString(),   0, 205, 205));
-	qualityFailures.addMarker(IntervalMarker::Marker("ILM_lower"     , tr("ILM lower"               ).toStdString(), 255, 255,   0));
-	qualityFailures.addMarker(IntervalMarker::Marker("BM_upper"      , tr("BM upper"                ).toStdString(), 240, 110,  90));
-	qualityFailures.addMarker(IntervalMarker::Marker("BM_lower"      , tr("BM lower"                ).toStdString(), 255, 140,   0));
-	qualityFailures.addMarker(IntervalMarker::Marker("SegILMmissing" , tr("missing ILM segmentation").toStdString(), 255,   0,   0));
-	qualityFailures.addMarker(IntervalMarker::Marker("SegBMmissing"  , tr("missing BM segmentation" ).toStdString(), 255, 200,   0));
+	qualityFailures.addMarker(IntervalMarker::Marker("signalmissing" , tr("signal missing"          ).toStdString(), 255, 255,   0));
+	qualityFailures.addMarker(IntervalMarker::Marker("ILM_upper"     , tr("ILM upper"               ).toStdString(), 255,   0,   0));
+	qualityFailures.addMarker(IntervalMarker::Marker("ILM_lower"     , tr("ILM lower"               ).toStdString(), 240, 110,  90));
+	qualityFailures.addMarker(IntervalMarker::Marker("BM_upper"      , tr("BM upper"                ).toStdString(), 255, 140,   0));
+	qualityFailures.addMarker(IntervalMarker::Marker("BM_lower"      , tr("BM lower"                ).toStdString(), 255, 200,   0));
+	qualityFailures.addMarker(IntervalMarker::Marker("SegILMmissing" , tr("missing ILM segmentation").toStdString(),   0, 205, 205));
+	qualityFailures.addMarker(IntervalMarker::Marker("SegBMmissing"  , tr("missing BM segmentation" ).toStdString(),   0, 140, 140));
 	qualityFailures.addMarker(IntervalMarker::Marker("MultiError"    , tr("multi error"             ).toStdString(), 138,  43, 226));
 
 	intervallMarkerMap.emplace("qualityFailures", qualityFailures);
