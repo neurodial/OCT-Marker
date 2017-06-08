7aa515187a2b6b60412cb711ce75df1c88227b5d
Kay Gawlik <kaydev@amarunet.de>
2017-06-09 00:57:13 +0200

    Schichtensegmentierung: Einfaches Mal-Gerüst gebaut

--- src/markermodules/bscanlayersegmentation/bscanlayersegmentation.cpp
+++ src/markermodules/bscanlayersegmentation/bscanlayersegmentation.cpp
@@ -1,5 +1,13 @@
 #include "bscanlayersegmentation.h"
 
+#include <widgets/bscanmarkerwidget.h>
+#include <QPainter>
+
+#include <octdata/datastruct/series.h>
+#include <octdata/datastruct/bscan.h>
+#include <QMouseEvent>
+#include <data_structure/programoptions.h>
+
 BScanLayerSegmentation::BScanLayerSegmentation(OctMarkerManager* markerManager)
 : BscanMarkerBase(markerManager)
 {
@@ -8,3 +16,170 @@ BScanLayerSegmentation::BScanLayerSegmentation(OctMarkerManager* markerManager)
 	icon = QIcon(":/icons/seglinelayer_edit.png");
 
 }
+
+namespace
+{
+	void paintSegmentationLine(QPainter& segPainter, int bScanHeight, const OctData::Segmentationlines::Segmentline& segLine, double factor)
+	{
+		double lastEnt = std::numeric_limits<OctData::Segmentationlines::SegmentlineDataType>::quiet_NaN();
+		int xCoord = 0;
+		for(OctData::Segmentationlines::SegmentlineDataType value : segLine)
+		{
+			// std::cout << value << '\n';
+			if(!std::isnan(lastEnt) && lastEnt < bScanHeight && lastEnt > 0 && value < bScanHeight && value > 0)
+			{
+				segPainter.drawLine(QLineF((xCoord-1)*factor, lastEnt*factor, xCoord*factor, value*factor));
+			}
+			lastEnt = value;
+			++xCoord;
+		}
+	}
+
+}
+
+
+QRect BScanLayerSegmentation::getWidgetPaintSize(const BScanLayerSegmentation::SegPoint& p1, const BScanLayerSegmentation::SegPoint& p2, double scaleFactor)
+{
+	const int broder = 5;
+
+	int minX = static_cast<int>(std::min(p1.x, p2.x)*scaleFactor) - broder;
+	int maxX = static_cast<int>(std::max(p1.x, p2.x)*scaleFactor) + broder;
+
+	int minY = static_cast<int>(std::min(p1.y, p2.y)*scaleFactor) - broder;
+	int maxY = static_cast<int>(std::max(p1.y, p2.y)*scaleFactor) + broder;
+
+// 	QRect rect = QRect(minX, minY, maxX-minX, maxY-minY);
+	QRect rect = QRect(minX, 0, maxX-minX, static_cast<int>(getBScanHight()*scaleFactor+0.5)); // old and new pos
+
+	return rect;
+}
+
+
+
+void BScanLayerSegmentation::drawMarker(QPainter& painter, BScanMarkerWidget* widget, const QRect&) const
+{
+	int bScanHeight = getBScanHight();
+	double scaleFactor = widget->getImageScaleFactor();
+
+
+	QPen pen;
+	pen.setColor(Qt::red);
+	pen.setWidth(4);
+	painter.setPen(pen);
+
+	paintSegmentationLine(painter, bScanHeight, lines[getActBScanNr()].getSegmentLine(OctData::Segmentationlines::SegmentlineType::ILM  ), scaleFactor);
+}
+
+BScanLayerSegmentation::SegPoint BScanLayerSegmentation::calcPoint(int x, int y, double scaleFactor, int bscanWidth)
+{
+	if(x<0)
+		x = 0;
+
+	std::size_t xTransformed = static_cast<std::size_t>(x/scaleFactor);
+	double      yTransformed =                          y/scaleFactor ;
+
+	if(xTransformed>bscanWidth-1)
+		xTransformed = static_cast<std::size_t>(bscanWidth-1);
+
+	return SegPoint(xTransformed, yTransformed);
+}
+
+void BScanLayerSegmentation::setLinePoint2Point(const BScanLayerSegmentation::SegPoint& p1, const BScanLayerSegmentation::SegPoint& p2, OctData::Segmentationlines::Segmentline& segLine)
+{
+	if(p1.x == p2.x)
+	{
+		segLine[p2.x] = p2.y;
+		return;
+	}
+
+	std::size_t pMin;
+	std::size_t pMax;
+	double pMinY;
+	double pMaxY;
+
+	if(p1.x < p2.x)
+	{
+		pMin = p1.x;
+		pMax = p2.x;
+		pMinY = p1.y;
+		pMaxY = p2.y;
+	}
+	else
+	{
+		pMin = p2.x;
+		pMax = p1.x;
+		pMinY = p2.y;
+		pMaxY = p1.y;
+	}
+
+	std::size_t length = pMax - pMin;
+	double lengthD = static_cast<double>(length);
+
+	for(std::size_t pos = 0; pos <= length; ++pos)
+	{
+		segLine[pos+pMin] = pMinY*(1. - pos/lengthD) + pMaxY*(pos/lengthD);
+	}
+// 	qDebug("%lf", segLine[length+pMin]);
+}
+
+
+BscanMarkerBase::RedrawRequest BScanLayerSegmentation::mouseMoveEvent(QMouseEvent* event, BScanMarkerWidget* widget)
+{
+	if(!paintSegLine)
+		return BscanMarkerBase::RedrawRequest();
+
+	double scaleFactor = widget->getImageScaleFactor();
+	SegPoint segPoint = calcPoint(event->x(), event->y(), scaleFactor, getBScanWidth());
+
+	OctData::Segmentationlines::Segmentline& segLine = lines[getActBScanNr()].getSegmentLine(OctData::Segmentationlines::SegmentlineType::ILM);
+
+	setLinePoint2Point(lastPoint, segPoint, segLine);
+
+	BscanMarkerBase::RedrawRequest request;
+	request.redraw = true;
+	request.rect = getWidgetPaintSize(lastPoint, segPoint, scaleFactor);
+
+	lastPoint = segPoint;
+	return request;
+}
+
+BscanMarkerBase::RedrawRequest BScanLayerSegmentation::mousePressEvent(QMouseEvent* event, BScanMarkerWidget* widget)
+{
+	paintSegLine = true;
+	double scaleFactor = widget->getImageScaleFactor();
+	lastPoint = calcPoint(event->x(), event->y(), scaleFactor, getBScanWidth());
+	return BscanMarkerBase::RedrawRequest();
+}
+
+BscanMarkerBase::RedrawRequest BScanLayerSegmentation::mouseReleaseEvent(QMouseEvent*, BScanMarkerWidget*)
+{
+	paintSegLine = false;
+	return BscanMarkerBase::RedrawRequest();
+}
+
+void BScanLayerSegmentation::newSeriesLoaded(const OctData::Series* series, boost::property_tree::ptree& ptree)
+{
+	resetMarkers(series);
+
+}
+
+
+void BScanLayerSegmentation::resetMarkers(const OctData::Series* series)
+{
+	if(!series)
+		return;
+
+	const std::size_t numBscans = series->bscanCount();
+
+	lines.clear();
+	lines.resize(numBscans);
+
+	for(std::size_t i = 0; i<numBscans; ++i)
+	{
+		int bscanWidth = series->getBScan(i)->getWidth();
+
+		lines[i].getSegmentLine(OctData::Segmentationlines::SegmentlineType::ILM  ).resize(bscanWidth);
+	}
+
+}
+
--- src/markermodules/bscanlayersegmentation/bscanlayersegmentation.h
+++ src/markermodules/bscanlayersegmentation/bscanlayersegmentation.h
@@ -1,14 +1,46 @@
 #ifndef BSCANLAYERSEGMENTATION_H
 #define BSCANLAYERSEGMENTATION_H
 
+#include<octdata/datastruct/segmentationlines.h>
 
 #include "../bscanmarkerbase.h"
 
 class BScanLayerSegmentation : public BscanMarkerBase
 {
 	Q_OBJECT
+
+	struct SegPoint
+	{
+		SegPoint() : x(0), y(0) {}
+		SegPoint(std::size_t x, double y) : x(x), y(y) {}
+
+		std::size_t x;
+		double      y;
+	};
+	std::vector<OctData::Segmentationlines> lines;
+
+	void resetMarkers(const OctData::Series* series);
+
+	SegPoint lastPoint;
+	bool paintSegLine = false;
+
+	SegPoint calcPoint(int x, int y, double scaleFactor, int bscanWidth);
+
+	void setLinePoint2Point(const SegPoint& p1, const SegPoint& p2, OctData::Segmentationlines::Segmentline& segLine);
+
+	QRect getWidgetPaintSize(const SegPoint& p1, const SegPoint& p2, double scaleFactor);
+
 public:
 	BScanLayerSegmentation(OctMarkerManager* markerManager);
+
+	virtual void drawMarker(QPainter& painter, BScanMarkerWidget* widget, const QRect& /*drawrect*/) const;
+
+	virtual RedrawRequest mouseMoveEvent   (QMouseEvent*, BScanMarkerWidget*);
+	virtual RedrawRequest mousePressEvent  (QMouseEvent*, BScanMarkerWidget*);
+	virtual RedrawRequest mouseReleaseEvent(QMouseEvent*, BScanMarkerWidget*);
+
+
+	virtual void newSeriesLoaded(const OctData::Series* series, boost::property_tree::ptree& ptree);
 };
 
 #endif // BSCANLAYERSEGMENTATION_H
